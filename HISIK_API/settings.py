e83f732881884bfeced633e658436c92547aa9f8
Mostafa-3azima <tito68932@gmail.com>
2019-06-11 12:14:11 +0200

    CORS ENABLED enhance

--- HISIK_API/settings.py
+++ HISIK_API/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'user',
     'adminNotification',
     'UserNotification',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'HISIK_API.urls'
@@ -136,6 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST='http://localhost:8100',
 
 REST_FRAMEWORK = {
 
